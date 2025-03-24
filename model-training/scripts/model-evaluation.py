cfd31c6a8fcf1275b3169e75e3ca762f872729eb
Ashen Abeysekara <ashendul@gmail.com>
2025-03-24 09:42:06 +0530

    added model evaluation script and its imports

---
+++ model-training/scripts/model-evaluation.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import joblib
+import json
+import os
+from datetime import datetime
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, roc_curve, auc
